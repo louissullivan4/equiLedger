a36f0f5b1c099e25ac6f284a65435b7ebda50af3
Louis Sullivan <Louis.Sullivan1@ibm.com>
2024-08-31 15:00:01 +0100

    Rename env variable

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const connectionString = process.env.DB_CONNECTION_STRING;
+const connectionURL = process.env.DATABASE_URL;
 const pool = new Pool({
-    connectionString
+    connectionURL
 });
 
 // const pool = new Pool({
